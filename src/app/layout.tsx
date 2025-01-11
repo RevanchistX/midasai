720afb9f2975f9c7d97b95642a7f81e517680450
Revan <Revan_TSL@hotmail.com>
2025-01-11 20:33:27 +0100

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,33 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Password Generator",
-  description: "Generate unique passwords",
+    title: "Password Generator",
+    description: "Generate unique passwords",
+    openGraph: {
+        images: "./favico.ico"
+    }
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+export default function RootLayout({children,}: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    return (
+        <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
