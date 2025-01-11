996e1f6441a038fb8a651a734aeba071df40958b
Revan <Revan_TSL@hotmail.com>
2025-01-11 21:58:21 +0100

    fking cssssss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
         <div>
             <div>
                 <div
-                    className="bg-blue-800 shadow-2xl grid-cols-1 place-items-center text-xs md:text-xl rounded-b-xl drop-shadow-2xl md:pl-0 pl-5 pb-5 mb-5 mx-auto overflow-hidden">
+                    className="bg-blue-800 shadow-2xl grid-cols-1 place-items-center text-xs md:text-xl rounded-b-xl drop-shadow-2xl pb-5 mb-5 mx-auto overflow-hidden">
                     <Image src={icon} width={100} alt={"icon"}/>
                     <div className="text-3xl text-center"> Password Generator</div>
                     <ul className="text-center">
