6e2c1c235590a931e3cd3da5349cec91cd2b6a19
Revan <Revan_TSL@hotmail.com>
2025-01-12 18:28:11 +0100

    wtf is going on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
+import {Metadata} from "next";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -11,46 +12,19 @@ const geistMono = Geist_Mono({
     subsets: ["latin"],
 });
 
-// export const metadata: Metadata = {
-//     title: "Password Generator",
-//     description: "Generate unique passwords",
-//     openGraph: {
-//         images: ["https://midasai.vercel.app/favicon.ico"]
-//     }
-// };
+export const metadata: Metadata = {
+    title: "Password Generator",
+    description: "Generate unique passwords",
+    openGraph: {
+        images: ["https://midasai.vercel.app/favicon.ico"]
+    }
+};
 
 export default function RootLayout({children,}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="en">
-        <head>
-            <meta charSet="utf-8"/>
-            <title>Непокор.мк - Непокорот е слобода!</title>
-            <meta name="description"
-                  content="Најголем избор на македонски патриотски производи на едно место. Од македонски знамиња, македонски дресови, маици, дуксери, блузони до стикери, шишиња и други производи за патриоти. Посетете нè сега!"/>
-            <meta name="keywords"
-                  content="патриотизам, patriotizam, македонски знамиња, makedonski znaminja, kutles, кутлеш, знаме кутлеш, zname kutles, patrioti, патриоти, patiotsko drustvo, патриотско друштво, македонски дресови, makedonski dresovi, патриотски маици, patriotski maici"/>
-            <base href="/"/>
-            <meta property="og:type" content="website"/>
-            <meta property="og:title" content="Непокор.мк - Непокорот е слобода!"/>
-            <meta property="og:description"
-                  content="Најголем избор на македонски патриотски производи на едно место. Од македонски знамиња, македонски дресови, маици, дуксери, блузони до стикери, шишиња и други производи за патриоти. Посетете нè сега!"/>
-            <meta property="og:image" content="https://www.nepokor.mk/assets/logo/logo.png"/>
-            <meta property="og:image:type" content="image/png"/>
-            <meta property="og:image:width" content="3000"/>
-            <meta property="og:image:height" content="2409"/>
-            <meta property="og:url" content="https://nepokor.mk/"/>
-            <meta name="twitter:card" content="summary_large_image"/>
-            <meta name="twitter:title" content="Непокор.мк - Непокорот е слобода!"/>
-            <meta name="twitter:description"
-                  content="Најголем избор на македонски патриотски производи на едно место. Од македонски знамиња, македонски дресови, маици, дуксери, блузони до стикери, шишиња и други производи за патриоти. Посетете нè сега!"/>
-            <meta name="twitter:image" content="https://www.nepokor.mk/assets/logo/logo.png"/>
-            <meta name="twitter:image:type" content="image/png"/>
-            <meta name="twitter:image:width" content="3000"/>
-            <meta name="twitter:image:height" content="2409"/>
-            <meta name="twitter:url" content="https://nepokor.mk/"/>
-        </head>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         </body>
